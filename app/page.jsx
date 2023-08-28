2e5c154285858c40427d39af32488a685622e5d1
renatodavis <renatodavis@gmail.com>
2023-08-28 20:41:29 -0300

    Alterações na página principal

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
 
 const Home = () => {
   return (
-    <section className="w-full flex-col">Home</section>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Renato Davis
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient"> Analista Desenvolvedor </span>
+      </h1>
+    </section>
     
   )
 }
